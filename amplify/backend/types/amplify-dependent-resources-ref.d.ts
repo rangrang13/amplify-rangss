308ab5d6ababfe8df705413540f3a983b3c96d00
rangrang <lunaticmoon1113@naver.com>
2024-06-22 11:32:36 +0900

    initial commit

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,9 @@
+export type AmplifyDependentResourcesAttributes = {
+  "api": {
+    "rangss": {
+      "GraphQLAPIEndpointOutput": "string",
+      "GraphQLAPIIdOutput": "string",
+      "GraphQLAPIKeyOutput": "string"
+    }
+  }
+}
---
+++ subscriptions.js
@@ -0,0 +1,39 @@
+/* eslint-disable */
+// this is an auto generated file. This will be overwritten
+
+export const onCreateTweet = /* GraphQL */ `
+  subscription OnCreateTweet($filter: ModelSubscriptionTweetFilterInput) {
+    onCreateTweet(filter: $filter) {
+      id
+      text
+      author
+      createdAt
+      updatedAt
+      __typename
+    }
+  }
+`;
+export const onUpdateTweet = /* GraphQL */ `
+  subscription OnUpdateTweet($filter: ModelSubscriptionTweetFilterInput) {
+    onUpdateTweet(filter: $filter) {
+      id
+      text
+      author
+      createdAt
+      updatedAt
+      __typename
+    }
+  }
+`;
+export const onDeleteTweet = /* GraphQL */ `
+  subscription OnDeleteTweet($filter: ModelSubscriptionTweetFilterInput) {
+    onDeleteTweet(filter: $filter) {
+      id
+      text
+      author
+      createdAt
+      updatedAt
+      __typename
+    }
+  }
+`;
